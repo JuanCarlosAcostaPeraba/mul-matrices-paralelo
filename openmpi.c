8f55f2b6fdae3248e4e857a6654b6de916342df7
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-07-30 20:05:42 +0100

    Finish OpenMPI script

--- openmpi.c
+++ openmpi.c
@@ -9,160 +9,96 @@
 #include <time.h>
 
 // Constantes
-#define N 2
+#define N 6
 #define CLOCKS_PER_SEC 1000000
 
-// Función para inicializar una matriz con valores aleatorios
-void initializeMatrix(float** matrix, int rows, int cols) {
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < cols; j++) {
-			matrix[i][j] = (float)rand() / RAND_MAX; // Valores aleatorios entre 0 y 1
+// Funciones
+void fill_matrix(int matrix[N][N]) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			matrix[i][j] = rand() % 10;
 		}
 	}
 }
 
-
-// Función para mostrar una matriz
-void displayMatrix(float** matrix, int rows, int cols) {
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < cols; j++) {
-			printf("%.2f\t", matrix[i][j]);
+void print_matrix(int matrix[N][N]) {
+	// Imprime la matriz
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			printf("%d ", matrix[i][j]);
 		}
 		printf("\n");
 	}
+	printf("\n");
 }
 
-// Función para multiplicar las filas de una matriz por una matriz B
-void multiplyRowsByMatrix(float** A, float** B, float** C, int rows_A, int cols_A, int cols_B) {
-	for (int i = 0; i < rows_A; i++) {
-		for (int j = 0; j < cols_B; j++) {
-			C[i][j] = 0.0;
-			for (int k = 0; k < cols_A; k++) {
-				C[i][j] += A[i][k] * B[k][j];
+void matrix_mult(int a[N][N], int b[N][N], int c[N][N]) {
+	// Multiplicación de dos matrices cuadradas
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			c[i][j] = 0;
+			for (int k = 0; k < N; k++) {
+				c[i][j] += a[i][k] * b[k][j];
 			}
 		}
 	}
 }
 
-// Función principal
-int main(int argc, char** argv) {
-	int num_procs, rank;
+int main(int argc, char *argv[]) {
+	// Declaración de variables
+	int rank, size;
 
 	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	// Tamaño de las matrices
-	int rows_A = N; // Tamaño de filas de la matriz A
-	int cols_A = N; // Tamaño de columnas de la matriz A
-	int cols_B = N; // Tamaño de columnas de la matriz B
+	// Empezar contador de tiempo
+	clock_t start = clock(); // CPU
+	double start_mpi = MPI_Wtime(); // MPI
 
-	// Verificar que el número de procesos sea mayor o igual al número de filas de la matriz A
-	if (num_procs < rows_A) {
+	if (size != N) {
 		if (rank == 0) {
-			printf("Error: El número de procesos debe ser mayor o igual al número de filas de la matriz A.\n");
+			printf("Este programa está diseñado para funcionar %d procesos MPI.\n", N);
 		}
 		MPI_Finalize();
 		return 1;
 	}
 
-	// Reservar memoria para las matrices A, B y C
-	float** A = NULL;
-	float** B = NULL;
-	float** C = NULL;
+	int matrix_A[N][N], matrix_B[N][N], matrix_C[N][N];
 
-	// El proceso 0 se encargará de la matriz A y la matriz resultante C
 	if (rank == 0) {
-			A = (float**)malloc(rows_A * sizeof(float*));
-			C = (float**)malloc(rows_A * sizeof(float*));
-			for (int i = 0; i < rows_A; i++) {
-				A[i] = (float*)malloc(cols_A * sizeof(float));
-				C[i] = (float*)malloc(cols_B * sizeof(float));
-			}
-			// Inicializar matriz A con valores aleatorios
-			initializeMatrix(A, rows_A, cols_A);
-			printf("Matriz A:\n");
-			displayMatrix(A, rows_A, cols_A);
-	}
+		// Llenar las matrices A y B solo en el proceso 0
+		fill_matrix(matrix_A);
+		fill_matrix(matrix_B);
 
-	// Todos los procesos crean la matriz B
-	B = (float**)malloc(cols_A * sizeof(float*));
-	for (int i = 0; i < cols_A; i++) {
-		B[i] = (float*)malloc(cols_B * sizeof(float));
-	}
+		printf("\nMatriz A:\n");
+		print_matrix(matrix_A);
 
-	// Inicializar matriz B con valores aleatorios en el proceso 0
-	if (rank == 0) {
-		initializeMatrix(B, cols_A, cols_B);
 		printf("\nMatriz B:\n");
-		displayMatrix(B, cols_A, cols_B);
+		print_matrix(matrix_B);
 	}
 
-	// Todos los procesos calculan la multiplicación de sus filas de A por la matriz B
-	float** local_C = (float**)malloc(rows_A * sizeof(float*));
-	for (int i = 0; i < rows_A; i++) {
-		local_C[i] = (float*)malloc(cols_B * sizeof(float));
-	}
+	// Transmitir las matrices A y B a todos los procesos
+	MPI_Bcast(&matrix_A[0][0], N * N, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&matrix_B[0][0], N * N, MPI_INT, 0, MPI_COMM_WORLD);
 
-	// Comunicación de la matriz B a todos los procesos
-	MPI_Bcast(&(B[0][0]), cols_A * cols_B, MPI_FLOAT, 0, MPI_COMM_WORLD);
+	// Multiplicación de matrices
+	matrix_mult(matrix_A, matrix_B, matrix_C);
 
-	// Reparto de filas de A a los procesos
-	int local_rows = rows_A / num_procs;
-	int remaining_rows = rows_A % num_procs;
-	int* send_counts = (int*)malloc(num_procs * sizeof(int));
-	int* displs = (int*)malloc(num_procs * sizeof(int));
+	// Recopilar resultados en el proceso 0
+	MPI_Gather(&matrix_C[0][0], N * N, MPI_INT, &matrix_C[0][0], N * N, MPI_INT, 0, MPI_COMM_WORLD);
 
-	for (int i = 0; i < num_procs; i++) {
-		send_counts[i] = (i < remaining_rows) ? local_rows + 1 : local_rows;
-		displs[i] = (i > 0) ? displs[i - 1] + send_counts[i - 1] : 0;
-	}
-
-	// Reparto de filas de A
-	float* send_buffer = (float*)malloc(send_counts[rank] * cols_A * sizeof(float));
-	MPI_Scatterv(&(A[0][0]), send_counts, displs, MPI_FLOAT, send_buffer, send_counts[rank] * cols_A, MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-	// Cálculo de la multiplicación local
-	float** local_A = (float**)malloc(send_counts[rank] * sizeof(float*));
-	for (int i = 0; i < send_counts[rank]; i++) {
-		local_A[i] = &(send_buffer[i * cols_A]);
-	}
-
-	multiplyRowsByMatrix(local_A, B, local_C, send_counts[rank], cols_A, cols_B);
-
-	// Recopilación de los resultados parciales en el proceso 0
-	MPI_Gatherv(&(local_C[0][0]), send_counts[rank] * cols_B, MPI_FLOAT, &(C[0][0]), send_counts, displs, MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-	// El proceso 0 muestra el resultado final
 	if (rank == 0) {
-		printf("\nResultado de la multiplicación (Distribuido):\n");
-		displayMatrix(C, rows_A, cols_B);
+		printf("\nMatriz C (resultado):\n");
+		print_matrix(matrix_C);
 	}
 
-	// Liberar memoria
-	if (rank == 0) {
-		for (int i = 0; i < rows_A; i++) {
-			free(A[i]);
-			free(C[i]);
-		}
-		free(A);
-		free(C);
-	}
-
-	for (int i = 0; i < cols_A; i++) {
-		free(B[i]);
-	}
-	free(B);
-
-	for (int i = 0; i < send_counts[rank]; i++) {
-		free(local_A[i]);
-		free(local_C[i]);
-	}
-	free(local_A);
-	free(local_C);
-	free(send_buffer);
-	free(send_counts);
-	free(displs);
+	// Imprimir tiempo de ejecución
+	printf("\nvvvvvvvvvvvv\n");
+	printf("Proceso %d de %d\n", rank, size);
+	printf("Tiempo de ejecución del programa (CPU): %f segundos\n", ((double) clock() - start) / CLOCKS_PER_SEC);
+	printf("Tiempo de ejecución del programa (MPI_Wtime): %f segundos\n", MPI_Wtime() - start_mpi);
+	printf("^^^^^^^^^^^^\n");
 
 	MPI_Finalize();
 
@@ -173,4 +109,4 @@ int main(int argc, char** argv) {
 /// mpicc openmpi.c -o openmpi
 /// Instrucciones para ejecutar el programa:
 /// mpirun -np X ./openmpi
-/// X es el número de procesos a utilizar
+/// X es el número de procesos a utilizar = N
