2075127f369d237fbe2cb831c19226d81221a399
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-08-01 13:36:57 +0100

    Update OpenMPI

--- openmpi.c
+++ openmpi.c
@@ -13,7 +13,7 @@
 #define CLOCKS_PER_SEC 1000000
 
 // Funciones
-void fill_matrix(int matrix[N][N]) {
+void rellenar_matriz(int matrix[N][N]) {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			matrix[i][j] = rand() % 10;
@@ -21,7 +21,7 @@ void fill_matrix(int matrix[N][N]) {
 	}
 }
 
-void print_matrix(int matrix[N][N]) {
+void imprimir_matriz(int matrix[N][N]) {
 	// Imprime la matriz
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
@@ -32,7 +32,7 @@ void print_matrix(int matrix[N][N]) {
 	printf("\n");
 }
 
-void matrix_mult(int a[N][N], int b[N][N], int c[N][N]) {
+void multiplicar_matrices(int a[N][N], int b[N][N], int c[N][N]) {
 	// Multiplicación de dos matrices cuadradas
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
@@ -44,6 +44,7 @@ void matrix_mult(int a[N][N], int b[N][N], int c[N][N]) {
 	}
 }
 
+// Función principal
 int main(int argc, char *argv[]) {
 	// Declaración de variables
 	int rank, size;
@@ -68,14 +69,14 @@ int main(int argc, char *argv[]) {
 
 	if (rank == 0) {
 		// Llenar las matrices A y B solo en el proceso 0
-		fill_matrix(matrix_A);
-		fill_matrix(matrix_B);
+		rellenar_matriz(matrix_A);
+		rellenar_matriz(matrix_B);
 
 		printf("\nMatriz A:\n");
-		print_matrix(matrix_A);
+		imprimir_matriz(matrix_A);
 
 		printf("\nMatriz B:\n");
-		print_matrix(matrix_B);
+		imprimir_matriz(matrix_B);
 	}
 
 	// Transmitir las matrices A y B a todos los procesos
@@ -83,14 +84,14 @@ int main(int argc, char *argv[]) {
 	MPI_Bcast(&matrix_B[0][0], N * N, MPI_INT, 0, MPI_COMM_WORLD);
 
 	// Multiplicación de matrices
-	matrix_mult(matrix_A, matrix_B, matrix_C);
+	multiplicar_matrices(matrix_A, matrix_B, matrix_C);
 
 	// Recopilar resultados en el proceso 0
 	MPI_Gather(&matrix_C[0][0], N * N, MPI_INT, &matrix_C[0][0], N * N, MPI_INT, 0, MPI_COMM_WORLD);
 
 	if (rank == 0) {
 		printf("\nMatriz C (resultado):\n");
-		print_matrix(matrix_C);
+		imprimir_matriz(matrix_C);
 	}
 
 	// Imprimir tiempo de ejecución
